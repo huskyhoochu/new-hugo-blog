ff2482d66b1f48e962316e868baf67823d749461
huskyhoochu <dfg1499@gmail.com>
2019-03-16 13:46:49 +0900

    Add now config

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,6 @@
 #! /usr/bin/env bash
 
 yarn build
-cd ./public
-cp -r . ../../huskyhoochu.github.io/
-cd ../../huskyhoochu.github.io
-git add .
-
-# Commit changes.
-msg="rebuilding site `date '+%Y-%m-%d %H:%M:%S'`"
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
+cp now.json public
+cd public
+now
