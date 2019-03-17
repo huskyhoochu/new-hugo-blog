f568103bc945616d560aea1374a15d6f3c5e7b73
huskyhoochu <dfg1499@gmail.com>
2019-03-17 15:22:09 +0900

    Modify echo formatting

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env bash
 
+echo -e '\n\033[1;32mDeploying Start...\033[0m\n'
+
 now
+
+echo -e '\n\033[1;32mDone.\n\nCurrent deploy statement is:\033[0m\n'
 
 now ls hugo-blog
 
@@ -10,10 +14,16 @@ old_url=`echo ${list_url} | cut -d ' ' -f15`
 
 new_url=`echo ${list_url} | cut -d ' ' -f9`
 
-echo -e "\nOld URL is:" ${old_url} "\n"
+echo -e '\nNew URL is: \033[1;35m' ${new_url} '\033[0m\n'
+
+now alias ${new_url} www.huskyhoochu.com
+
+echo -e '\nOld URL is: \033[1;35m' ${old_url} '\033[0m\n'
 
 now rm -y ${old_url}
 
-echo -e "\nNew URL is:" ${new_url} "\n"
+echo -e '\n\033[1;32mDone.\n\nFinal deploy statement is:\033[0m\n'
 
-now alias ${new_url} www.huskyhoochu.com
+now ls hugo-blog
+
+echo -e '\n\033[1;32mAll process is done.\033[0m\n'
