91a921b2cda3c03043325ea70b77b31e622b63a3
huskyhoochu <dfg1499@gmail.com>
2019-03-17 15:56:09 +0900

    Add clean install command after deployed

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ yum install -y wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.54.0/hugo_0.54.0_Linux-64bit.tar.gz
 tar -xzf hugo_0.54.0_Linux-64bit.tar.gz
 
-./hugo
+yarn install
+rm -rf public
+rm -rf resources
+./hugo --gc
